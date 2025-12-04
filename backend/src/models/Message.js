9546f83ca6faa4a9938cbd04871a38772509d553
Saatvik Chauhan <saatvikchauhan03@gmail.com>
2025-12-04 19:24:13 +0530

    Remove auto-delete messages

--- backend/src/models/Message.js
+++ backend/src/models/Message.js
@@ -8,16 +8,9 @@ const MessageSchema = new mongoose.Schema(
     content: { type: String, required: true },
     likes: { type: [String], default: [] },
     edited: { type: Boolean, default: false },
-    expiresAt: {
-      type: Date,
-      default: () => new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-    },
   },
   { timestamps: true }
 );
-
-MessageSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
-// delete the message as soon as the expiresAt is reached
 
 const Message = mongoose.model("Message", MessageSchema);
 export default Message;
