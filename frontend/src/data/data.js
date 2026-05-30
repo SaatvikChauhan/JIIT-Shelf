cc518d6e1a542bf2be8b65e049c3eb6f2087cb16
Saatvik Chauhan <saatvikchauhan03@gmail.com>
2026-05-30 06:45:49 +0530

    Add sem 4 material

--- frontend/src/data/data.js
+++ frontend/src/data/data.js
@@ -1,8 +1,8 @@
 export const branchSemMap = {
-  "CSE": [1, 2, 3],
+  "CSE": [1, 2, 3, 4],
   "ECE": [1, 2],
-  "IT": [1, 2, 3],
-  "Mathematics and Computing (M&C)": [1, 2, 3],
+  "IT": [1, 2, 3, 4],
+  "Mathematics and Computing (M&C)": [1, 2, 3, 4],
   "Robotics and Artificial Intelligence": [1, 2],
 };
 
@@ -10,6 +10,7 @@ export const semIdMap = {
   "1": "1wcqXk9t5TfZjwpW2rvjBMKXwwWIrQ4ca",
   "2": "1Rb23Q-_-hZ0NCNPNlPJ9e4px2-wikiVB",
   "3": "1mfEfqFWJ3NkeYmdjM1-YED0GZ9q3Qvt2",
+  "4": "1A9FLeKbVnJXS3W5h9iUi2s6tvxqQdC84",
 };
 
 export const iconMap = {
